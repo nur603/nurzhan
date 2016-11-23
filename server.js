dad224bede9d04bb365e046c27002499a17bcc5f
nur603 <96_nur@mail.ru>
2016-11-23 11:04:11 +0600

    Update server.js

--- server.js
+++ server.js
@@ -13,21 +13,21 @@ app.get('/', function(req, res){
 	res.send('library');
 });
 
-// GET /books
-app.get ('/books', function(req, res){
-	var cost = 0;
-for (var i=0; i<books.length; i++)
-{
-	cost=i+1;
-}
-console.log(cost);
-res.send(JSON.stringify(books,null,null,10)+"\n Summ: "+cost+" books");
-	res.json(books);
-}
-);
+		// GET /books
+		app.get ('/books', function(req, res){
+		var col = 0;
+		for (var i=0; i<books.length; i++)
+		{
+		col=i+1;
+		}
+		console.log(col);
+		res.send(JSON.stringify(books,null,null,10)+"\n Summ: "+col+" books");
+		res.json(books);
+		}
+		);
 
-// GET /books/:id
-app.get('/books/id', function(req, res){  
+		// GET /books/:id
+		app.get('/books/id', function(req, res){  
 	var booksId = parseInt(req.books.id, 10);
 	var matchedbooks = _.findWhere(books, {id: booksId});
 
@@ -38,33 +38,31 @@ app.get('/books/id', function(req, res){
 	}
 });
 
-// POST /books
+	// POST /books
 
-app.post('/books', function(req, res){
-	var body = _.pick(req.body, 'name', 'author', 'price');
+		app.post('/books', function(req, res){
+		var body = _.pick(req.body, 'name', 'author', 'price');
 
-	if(!_.isString(body.name) || !_.isString(body.author) || !_.isNumber(body.price) ){
-									return res.status(400).send();
-								}
-								body.id = booksNextId++;
-								books.push(body);
-								res.json(body);
+		if(!_.isString(body.name) || !_.isString(body.author) || !_.isNumber(body.price) ){
+			return res.status(400).send();
+				}
+				body.id = booksNextId++;
+				books.push(body);
+				res.json(body);
 								
-							});
-										//Delete/books/:id
-										app.delete('/books/:id', function(req,res){
-											var booksId = parseInt(req.params.id,10);
-											var matchedbooks = _.findWhere(books, {id: booksId});
-
-											if(!matchedbooks){
-												res.status(404).json("not found");
-											}else{
-												books = _.without(books, matchedbooks);
-												res.json(matchedbooks);
-											}
-											console.log('Method Delete for /books/:id working');
-										});
-
-											app.listen(PORT, function(){
-											console.log('Express listening on port ' + PORT + '!');
-											});
+				});
+	//Delete/books/:id	
+		app.delete('/books/:id', function(req,res){
+		var booksId = parseInt(req.params.id,10);
+		var matchedbooks = _.findWhere(books, {id: booksId});
+		if(!matchedbooks){
+		res.status(404).json("not found");
+		}else{
+		books = _.without(books, matchedbooks);
+		res.json(matchedbooks);
+		}
+		console.log('Method Delete for /books/:id working');
+		});
+		app.listen(PORT, function(){
+		console.log('Express listening on port ' + PORT + '!');
+		});
